cb5f255d4a3932e935148dd182c0d2f182e559c3
Kooreis <kooreis.official@gmail.com>
2022-08-14 03:00:00 -0500

    Initial setup of the Main class and main method. A variable 'length' is declared and initialized with a value of 10.

--- Random_Password_Generator.java
+++ Random_Password_Generator.java
@@ -1,28 +1,7 @@
-```java
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         int length = 10;
-        System.out.println(generatePassword(length));
-    }
-
-    static char[] generatePassword(int len) {
-        String capitalChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String smallChars = "abcdefghijklmnopqrstuvwxyz";
-        String numbers = "0123456789";
-        String symbols = "!@#$%^&*_=+-/.?<>)";
-
-        String values = capitalChars + smallChars + numbers + symbols;
-
-        Random random = new Random();
-
-        char[] password = new char[len];
-
-        for (int i = 0; i < len; i++) {
-            password[i] = values.charAt(random.nextInt(values.length()));
-        }
-        return password;
     }
 }
-```
