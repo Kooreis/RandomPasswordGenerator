6713785e1c8c3acf6e0b1470a113d053dab7caf7
Kooreis <kooreis.official@gmail.com>
2022-08-14 03:00:00 -0500

    In this update, the System namespace is imported and the main class Program is defined. The Main method is also defined and a console message asking for the length of the password is added.

--- Random_Password_Generator.cs
+++ Random_Password_Generator.cs
@@ -1,21 +1,9 @@
-```CSharp
 using System;
-using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.Write("Enter the length of the password: ");
-        int length = int.Parse(Console.ReadLine());
-        Console.WriteLine(GeneratePassword(length));
-    }
-
-    static string GeneratePassword(int length)
-    {
-        const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        return new string(Enumerable.Repeat(valid, length)
-          .Select(s => s[new Random().Next(s.Length)]).ToArray());
     }
 }
-```
