b8b85fd759cf45e14c20ceb9d7a9a36f3273a5e2
Kooreis <kooreis.official@gmail.com>
2022-08-15 02:00:00 -0500

    Defined a function 'password_generator' that generates a random password of a given length. The password can contain ascii letters, digits, and punctuation.

--- Random_Password_Generator.py
+++ Random_Password_Generator.py
@@ -1,2 +1,2 @@
-import string
-import random
+def password_generator(length=8, chars=string.ascii_letters + string.digits + string.punctuation):
+    return ''.join(random.choice(chars) for _ in range(length))
